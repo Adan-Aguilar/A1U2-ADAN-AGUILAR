b1f4ba53df3e8cd1609b891582d5bef04ea8c715
Adán Aguilar <adandaniel99@hotmail.es>
2020-09-13 19:49:13 -0500

    version 1

---
+++ A1U2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using A1U2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace A1U2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Resultado(Numeros n)
+        {
+            return View(n);
+        }
+    }
+}
---
+++ A1U2/Models/Numeros.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace A1U2.Models
+{
+    public class Numeros
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+
+        public int Suma()
+        {
+            return Numero1 + Numero2;
+        }
+    }
+}
